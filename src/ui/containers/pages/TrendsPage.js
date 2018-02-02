58be532ef30743863e369279e35db3db39d48bb2
Oliver Hayes <olliehayes96@gmail.com>
2018-02-02 20:40:29 +0000

    pie charts yasss

--- src/ui/containers/pages/TrendsPage.js
+++ src/ui/containers/pages/TrendsPage.js
@@ -4,6 +4,7 @@ import HeaderPage from "../layouts/HeaderPage";
 import {connect} from "react-redux";
 import {Col, Grid, Row} from "react-bootstrap";
 import {getTrendData, getTrends} from "../../../reducers/trends";
+import {Pie, PieChart, Tooltip} from "recharts";
 
 class TrendsPage extends Component {
 
@@ -23,6 +24,25 @@ class TrendsPage extends Component {
 
     render() {
         let {trends, trendData, loadingTrendData} = this.props.trends;
+
+        let sentimentData;
+        if (trendData !== undefined && trendData.length > 0) {
+            let names = [...new Set(trendData.map(tweet => tweet.sentiment.name))];
+            sentimentData = names.map(name => {
+                return {
+                    name: name,
+                    value: trendData.filter(tweet => tweet.sentiment.name.indexOf(name) > -1).length
+                }
+            })
+        }
+
+        const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+            {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
+            {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
+
+        const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
+            {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
+            {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
 
         return (
             <HeaderPage>
@@ -45,13 +65,25 @@ class TrendsPage extends Component {
                                 <div className="empty-box"><p>Select a trend on the left.</p></div>}
                                 {loadingTrendData === true &&
                                 <div className="empty-box"><i className="fa fa-5x fa-spin fa-cog"/></div>}
-                                {trendData !== undefined && trendData}
-                                <Row>
-                                    <h3>Summary</h3>
-                                    <div className="summary">
+                                {(trendData !== undefined && trendData.length > 0) &&
+                                <Grid fluid>
+                                    <Row>
+                                        <h3>Summary</h3>
+                                        <div className="summary">
+                                            <PieChart width={800} height={400}>
+                                                <Pie isAnimationActive={false} data={sentimentData} cx={200} cy={200}
+                                                     outerRadius={80} fill="#8884d8" label/>
+                                                <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80}
+                                                     fill="#82ca9d"/>
+                                                <Tooltip/>
+                                            </PieChart>
+                                        </div>
+                                    </Row>
+                                    <Row>
 
-                                    </div>
-                                </Row>
+                                    </Row>
+                                </Grid>
+                                }
                             </div>
                         </Col>
                     </Row>
@@ -62,7 +94,8 @@ class TrendsPage extends Component {
 }
 
 TrendsPage.propTypes = {
-    trends: []
+    trends: [],
+
 };
 
 let mapStateToProps = (state) => {
